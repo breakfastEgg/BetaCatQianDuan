dbb66d83f5dd0ccd57fe84eaddceb98d6a93e1a9
cxl <cxl@u-bim.com>
2018-05-04 11:41:39 +0800

    “

---
+++ gitConfig.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# git 缩写配置，按需修改
+# ps：山哥辣鸡
+
+git config --global alias.st status
+
+git config --global alias.ci commit
+
+git config --global alias.co checkout
+
+git config --global alias.br branch
+
+#cat ~/.gitconfig
