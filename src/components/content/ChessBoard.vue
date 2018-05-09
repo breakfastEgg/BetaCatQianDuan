4fa3923f9a8cedabf9aaca258d878dc09bd2dd11
cxl <cxl@u-bim.com>
2018-05-09 14:05:57 +0800

    美化棋盘

--- src/components/content/ChessBoard.vue
+++ src/components/content/ChessBoard.vue
@@ -1,83 +1,67 @@
-//  deb784棋盘颜色
 <style scoped>
-.chessPadding{
+.chess-padding{
   padding: 10px;
-  margin: 0 auto
+  margin: 0 auto;
+  display: block;
+  min-width: 300px;
+  min-height: 300px;
+  background-color: #deb784;
+  border: 2px solid black;
+  border-radius: 5px;
 }
 </style>
 <template>
-  <canvas class="chessPadding"></canvas>
+  <canvas class="chess-padding" @click=""></canvas>
 </template>
 <script>
-let chess = function (id) {
-  let canvas = document.querySelector(`#${id}`)
-  let context = canvas.getContext('2d')
-  let me = true
-  let chessBox = [] // 用于存放棋盘中落子的情况
+  const lineNum = 20;
+  const lineWidth = 2
+  // function oneStep (i, j, k) {
+  //   context.beginPath()
+  //   context.arc(15 + i * 30, 15 + j * 30, 13, 0, 2 * Math.PI) // 绘制棋子
+  //   let g = context.createRadialGradient(
+  //     15 + i * 30,
+  //     15 + j * 30,
+  //     13,
+  //     15 + i * 30,
+  //     15 + j * 30,
+  //     0
+  //   ) // 设置渐变
+  //   if (k) {
+  //     // k=true是黑棋，否则是白棋
+  //     g.addColorStop(0, '#0A0A0A') // 黑棋
+  //     g.addColorStop(1, '#636766')
+  //   } else {
+  //     g.addColorStop(0, '#D1D1D1') // 白棋
+  //     g.addColorStop(1, '#F9F9F9')
+  //   }
+  //   context.fillStyle = g
+  //   context.fill()
+  //   context.closePath()
+  // }
 
-  for (let i = 0; i < 15; i++) {
-    chessBox[i] = []
-    for (let j = 0; j < 15; j++) {
-      chessBox[i][j] = 0 // 初始值为0
-    }
-  }
-
-  function drawChessBoard () {
-    for (let i = 0; i < 15; i++) {
-      context.strokeStyle = '#D6D1D1'
-      context.moveTo(15 + i * 30, 15) // 垂直方向画15根线，相距30px
-      context.lineTo(15 + i * 30, 435)
-      context.stroke()
-      context.moveTo(15, 15 + i * 30) // 水平方向画15根线，相距30px;棋盘为14*14；
-      context.lineTo(435, 15 + i * 30)
-      context.stroke()
-    }
-  }
-
-  drawChessBoard() // 绘制棋盘
-
-  function oneStep (i, j, k) {
-    context.beginPath()
-    context.arc(15 + i * 30, 15 + j * 30, 13, 0, 2 * Math.PI) // 绘制棋子
-    let g = context.createRadialGradient(
-      15 + i * 30,
-      15 + j * 30,
-      13,
-      15 + i * 30,
-      15 + j * 30,
-      0
-    ) // 设置渐变
-    if (k) {
-      // k=true是黑棋，否则是白棋
-      g.addColorStop(0, '#0A0A0A') // 黑棋
-      g.addColorStop(1, '#636766')
-    } else {
-      g.addColorStop(0, '#D1D1D1') // 白棋
-      g.addColorStop(1, '#F9F9F9')
-    }
-    context.fillStyle = g
-    context.fill()
-    context.closePath()
-  }
-
-  chess.onclick = function (e) {
-    let x = e.offsetX // 相对于棋盘左上角的x坐标
-    let y = e.offsetY // 相对于棋盘左上角的y坐标
-    let i = Math.floor(x / 30)
-    let j = Math.floor(y / 30)
-    if (chessBox[i][j] === 0) { // 原来是==
-      oneStep(i, j, me)
-      if (me) {
-        chessBox[i][j] = 1
-      } else {
-        chessBox[i][j] = 2
-      }
-      me = !me // 下一步白棋
-    }
-  }
-}
+  // chess.onclick = function (e) {
+  //   let x = e.offsetX // 相对于棋盘左上角的x坐标
+  //   let y = e.offsetY // 相对于棋盘左上角的y坐标
+  //   let i = Math.floor(x / 30)
+  //   let j = Math.floor(y / 30)
+  //   if (chessBox[i][j] === 0) { // 原来是==
+  //     oneStep(i, j, me)
+  //     if (me) {
+  //       chessBox[i][j] = 1
+  //     } else {
+  //       chessBox[i][j] = 2
+  //     }
+  //     me = !me // 下一步白棋
+  //   }
+  // }
 
 export default {
+  data () {
+    return {
+      chessBox: [lineNum - 1][lineNum - 1] // 落子情况
+    }
+  },
   mounted () {
     this.$nextTick(function () {
       this.resizeCanvas()
@@ -90,7 +74,7 @@ export default {
     resizeCanvas () {
       const paddingOfCanvas = 10
       const parentPadding = 16
-
+    
       let context = this.$el.getContext('2d')
       let offset
       let maxLength
@@ -101,19 +85,25 @@ export default {
       chessWidth = this.$parent.$el.clientWidth - 2 * (parentPadding + paddingOfCanvas)
 
       maxLength = chessHeight >= chessWidth ? chessWidth : chessHeight
-      this.$el.height = this.$el.width = maxLength
+      this.$el.setAttribute('width', maxLength)
+      this.$el.setAttribute('height', maxLength)
+      // this.$el.height = this.$el.width = maxLength
 
-      offset = maxLength / 14
+      offset = maxLength / (lineNum - 1)
 
-      for (let i = 0; i < 15; i++) {
-        context.strokeStyle = '#D6D1D1'
+      context.lineWidth = lineWidth
+      for (let i = 0; i < lineNum; i++) {
+        context.strokeStyle = '#fff'
         context.moveTo(i * offset, 0)
-        context.lineTo(i * offset, offset * 14)
+        context.lineTo(i * offset, offset * (lineNum - 1))
         context.stroke()
         context.moveTo(0, i * offset)
-        context.lineTo(offset * 14, i * offset)
+        context.lineTo(offset * (lineNum - 1), i * offset)
         context.stroke()
       }
+    },
+    goChecss () {
+
     }
   }
 }
